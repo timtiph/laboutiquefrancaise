913587f4a822c0384a44e7e1326d9b06d665b81a
timtiph <tiphany.moine@outlook.com>
2023-01-26 11:11:15 +0100

    pages one & all produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,15 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
+    //injection de dependance : tu prends l'entityManager pour entrer dans ce controller
+    // Appel entityManager pour récup tous les produits
+    private $entityManager;
+
+    // intialise un constructeur
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/nos-produits', name: 'app_products')]
     public function index(): Response
     {
-        return $this->render('product/index.html.twig');
+        //pour aller chercher de data : utiliser le repository associé => product repository (fichier permettant l'acces rapide aux données)
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        // dd($products); => $products est chargée grace à findAll() qui est égal à "SELECT * FROM product"</g>
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products, // passer tous les produits à twig
+        ]);
+    }
+
+    // fonction permettant de rentrer dans le produit avec un clic
+    // dans la mesure ou cette fonction sera directement liée au produit, il est possible de créer une 2eme fonction dans le ProductController (cf SEGMENTATION)
+
+    #[Route('/produit/{slug}', name: 'app_product')] // {ajout du slug} pour affichage d'une URL propre avec /produit/nom-du-produit
+
+    public function show($slug): Response
+    {
+        // dd($slug); // récup le slug de l'URL
+
+        //pour aller chercher de data : utiliser le repository associé => product repository (fichier permettant l'acces rapide aux données)
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        // dd($product); //=> $product est chargée grace à findOneBySlug() qui est égal à "SELECT * FROM product WHERE slug = $slug"</g>
+
+        if(!$product) { // si tu ne trouve pas de produit, redirect to app_products
+            return $this->redirectToRoute('app_products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product, // passer tous les produits à twig
+        ]);
     }
 }
