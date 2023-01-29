779cf476e8f2c148eab0676854b3097246b0786b
timtiph <tiphany.moine@outlook.com>
2023-01-29 19:25:00 +0100

    formulaire commande/validation adresses/carrier

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
     #[Route('/commande', name: 'app_order')]
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
         if (!$this->getUser()->getAddresses()->getValues()) {
             return $this->redirectToRoute('app_account_address_add');
@@ -20,9 +22,17 @@ class OrderController extends AbstractController
             'user' => $this->getUser()
         ]);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+
+        };
+
 
         return $this->render('order/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'cart' => $cart->getFull()
         ]);
     }
 }
