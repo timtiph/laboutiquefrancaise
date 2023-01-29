77d05ef5dd89480b7eaab1290aec85e6b351ca2f
timtiph <tiphany.moine@outlook.com>
2023-01-29 06:45:25 +0100

    Revert "Revert "function getFull + decrease""
    This reverts commit 177299bba71c249e54404996d814c94c1c196031.

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -3,6 +3,8 @@
 namespace App\Classe;
 // Code Symfony 5
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 //use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
@@ -12,12 +14,14 @@ class Cart
 {
     //private $session;
     private $requestStack;
+    private $entityManager;
 
     //public function __construct(SessionInterface $session)
-    public function __construct(RequestStack $requestStack)
+    public function __construct(EntityManagerInterface $entityManager, RequestStack $requestStack)
     {
         //$this->session = $session;
         $this->requestStack = $requestStack;
+        $this->entityManager = $entityManager;
     }
 
 
@@ -71,10 +75,67 @@ class Cart
 
         // récup l'objet cart dans un tableau, sinon renvoi un tableau vide
         $cart = $session->get('cart', []);
-        
+
         // on eleve l'entrée dans $cart qui a l'id $id
         unset($cart[$id]);
 
         return $this->requestStack->getSession()->set('cart', $cart);
+    }
+
+    /**
+     * @id du produit
+     * function pour retirer une quantité à un produit sur la page templates/cart/index.html.twig
+     */
+
+    public function decrease($id)
+    {
+        // vérifier si quantity = 1 > si oui, produit à supprimer 
+        // récup la session en cours
+        $session = $this->requestStack->getSession();
+
+        // récup l'objet cart
+        $cart = $session->get('cart', []);
+
+        if ($cart[$id] > 1) {
+            // retirer une quantité (-1)
+            $cart[$id]--;
+        } else {
+            // supprimer le produit
+            unset($cart[$id]);
+        }
+
+        return $this->requestStack->getSession()->set('cart', $cart);
+    }
+
+    public function getFull()
+    {
+        $cartComplete = [];
+
+        if ($this->get()) {
+
+            // Enrichir le cartComplete de datas pris par Doctrine en BDD
+            // as $id(clé) => $qunatity(valeur)
+            foreach ($this->get() as $id => $quantity) {
+
+                $product_object = $this->entityManager->getRepository(Product::class)->findOneById($id);
+                // a chaque itération, injete dans $cartComplete[] une nouvelle entrée avec plusieurs parametres
+                
+                // si on essai de passer un id qui n'est pas dans la base
+                // suppression du produit non existant de la session cart
+                if (!$product_object) {
+                    $this->delete($id);
+                    continue;
+                }
+                
+                $cartComplete[] = [
+
+                    // produit venant de la BDD via Doctrine et EntityManager intialisé par __construct, recherche du produit par $id produit
+                    'product' => $product_object,
+                    'quantity' => $quantity
+                ];
+            }
+        }
+
+        return $cartComplete;
     }
 }
