e76f8aa8c9dd88dc143152d07551492f930b7215
timtiph <tiphany.moine@outlook.com>
2023-02-01 14:58:56 +0100

    OrderSuccessController

--- src/Controller/OrderSuccessController.php
+++ src/Controller/OrderSuccessController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Entity\Order;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,7 +19,7 @@ class OrderSuccessController extends AbstractController
     }
 
     #[Route('/commande/merci/{stripeSessionId}', name: 'app_order_validate')]
-    public function index($stripeSessionId): Response
+    public function index($stripeSessionId, Cart $cart): Response
     {
 
         $order = $this->entityManager->getRepository(Order::class)->findOneByStripeSessionId($stripeSessionId);
@@ -28,13 +29,15 @@ class OrderSuccessController extends AbstractController
         }
 
         if (!$order->isIsPaid()){ // modifier le statut isPaid à 1 SAUF s'il est deja à 1
+            // vider la session cart
+            $cart->remove();
             // Modifier le statut isPaid de la commande en mettant 1. Systeme de webhook via stripe avec "ok le paiement est bien passé", donc tu peux appeler une URL
             $order->setIsPaid(1);
             $this->entityManager->flush();
             // envoyer un mail au client pour lui confirmer sa commande suite à la validation du paiement
         }
         
-        return $this->render('order_validate/index.html.twig', [
+        return $this->render('order_success/index.html.twig', [
             // Afficher quelques information de la commande de l'utilisateur
             'order' => $order // on passe l'obhet order à la vue
         ]);
