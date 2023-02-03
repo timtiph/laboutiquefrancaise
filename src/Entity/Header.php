838a6e3dbf41db1f38a7ee6a05df3b7034302169
timtiph <tiphany.moine@outlook.com>
2023-02-03 14:28:38 +0100

    Nouvelle Entity Header pour la gestion du bandeau carousel

---
+++ src/Entity/Header.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HeaderRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HeaderRepository::class)]
+class Header
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $btnTitle = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $btnURL = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Illustration = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getBtnTitle(): ?string
+    {
+        return $this->btnTitle;
+    }
+
+    public function setBtnTitle(string $btnTitle): self
+    {
+        $this->btnTitle = $btnTitle;
+
+        return $this;
+    }
+
+    public function getBtnURL(): ?string
+    {
+        return $this->btnURL;
+    }
+
+    public function setBtnURL(string $btnURL): self
+    {
+        $this->btnURL = $btnURL;
+
+        return $this;
+    }
+
+    public function getIllustration(): ?string
+    {
+        return $this->Illustration;
+    }
+
+    public function setIllustration(string $Illustration): self
+    {
+        $this->Illustration = $Illustration;
+
+        return $this;
+    }
+}
