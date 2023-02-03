4b700a2d49b714440db056adc00fe02de053a234
timtiph <tiphany.moine@outlook.com>
2023-02-03 15:45:37 +0100

    Affichage du carroussel + gestion dans easyAdmin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Classe\Mail;
+use App\Entity\Header;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,8 +24,12 @@ class HomeController extends AbstractController
     {
         $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
         // dd($product); // ok, les IsBest remontent
+
+        $headers = $this->entityManager->getRepository(Header::class)->findAll();
+
         return $this->render('home/index.html.twig', [
             'products' => $products,
+            'headers' => $headers
         ]);
     }
 }
