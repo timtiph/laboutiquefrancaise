f088ddd03584ae01805f6f456802659e369ac031
timtiph <tiphany.moine@outlook.com>
2023-02-04 18:20:21 +0100

    Ajout fonction  UpdatePassword + complément fonction Index

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use App\Entity\ResetPassword;
 use App\Entity\User;
+use App\Form\ResetPasswordType;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -45,9 +47,61 @@ class ResetPasswordController extends AbstractController
                 $this->entityManager->flush(); // 5 : flush le tout
 
                 // II : Envoyer un mail à l'utilisateur avec un lien lui permettant de mettre à jour son mot de passe 
+                $url = $this->generateUrl('app_update_password', [
+                    'token' => $reset_password->getToken()
+                ]);
+
+                $content = "Bonjour ".$user->getFirstname().", <br>Vous avez demander à réinitialiser votre mot de passe sur La Boutique Française.<br><br>";
+                $content .= "Merci de bien vouloir cliquer sur le lien suivant pour <a href='https://127.0.0.1:8000".$url."' class=''>mettre à jour votre mot depasse.</a>";
+                $mail = new Mail();
+                $mail->send($user->getEmail(), $user->getFirstname(), 'Réinitialiser votre mot de passe sur La Boutique Française.', $content);
+
+                $this->addFlash('notice', 'Un mail contenant la procédure de réinitialisation de votre mot de passe vous parviendra dans quelques instants.');
+            
+            } else {
+
+                $this->addFlash('notice', 'Cette adresse Email est inconnue.');
             }
         }
 
         return $this->render('reset_password/reset_password.html.twig');
     }
+
+
+    #[Route('/modifier-mon-mot-de-passe/{token}', name: 'app_update_password')]
+    public function update(Request $request, $token): Response
+    {
+        // dd($token); // ok, récupere bien le token avec l'adresse mail dans le mail envoyé
+        $reset_password = $this->entityManager->getRepository(ResetPassword::class)->findOneByToken($token); // on récupere le token et le user associé
+
+        if (!$reset_password) { // si variable n'existe pas, une erreur s'est produite donc redirection sur page reset_password
+            return $this->redirectToRoute('app_reset_password');
+        }
+
+        // dd($reset_password); // ok, récupere le dernier token et le User associé
+
+        $now = new DateTime(); // $now => dateTime courrente
+
+        // vérifier si le createdAt = now et encore valide suivant la durée de validité
+        if ($now > $reset_password->getCreatedAt()->modify('+ 3 hour')){ // $reset_password => date de la demande de réinitialisation + 3heures
+            // si $now > $reset_password + 3h alors => ERREUR le token à expiré
+            $this->addFlash('notice', 'Votre demande de mot de passe à expirée. Merci de la renouveller');
+            return $this->redirectToRoute('app_reset_password');
+        }; 
+        // si $now < $reset_password + 3h alors ok on modifie
+        // rendre une vue avec mot de passe et confirmer votre mot de passe
+
+        $form = $this->createForm(ResetPasswordType::class);
+        $form->handleRequest($request);
+        return $this->render('reset_password/update.html.twig');
+
+        // hash des mots de passe
+        // Flush en BDD
+        // redirection de l'utilisateur vers la page de connexion
+        
+        dd($reset_password);
+        
+    }
+
+
 }
